67d9b107bbb0b48a23f310abf8e210e5d7738221
DilaineRabelo <dilainefiuza@outlook.com>
2024-10-04 00:10:08 -0300

    atualização

--- src/Main.java
+++ src/Main.java
@@ -12,55 +12,57 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-
         Scanner ler = new Scanner(System.in);
-        int opcao = -1;
+        int opc = 1;
+        while (opc != 0) {
+            System.out.println("1 - Consultar cep:");
+            System.out.println("0 - Sair");
+            opc = ler.nextInt();
+            ler.nextLine();
+            try {
+                if (opc == 1) {
+                    System.out.println("Digite o CEP: ");
+                    String cep = ler.nextLine();
+                    //cep = cep.replaceAll("\\D", "");
+                    String endereco = "https://viacep.com.br/ws/" + cep + "/json/";
+                    Endereco meuEndereco = null;
 
+                    Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        System.out.println("Digite o cep:");
-        String cep = ler.nextLine();
-        cep = cep.replaceAll("\\D", "");
-        String endereco = "https://viacep.com.br/ws/" + cep + "/json/";
-        Endereco meuEndereco = null;
+                    HttpClient client = HttpClient.newHttpClient();
+                    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(URI.create(endereco))
+                            .build();
 
-        Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
+                    HttpResponse<String> response = client
+                            .send(request, HttpResponse.BodyHandlers.ofString());
 
+                    if (response.statusCode() == 400) {
+                        throw new Exception("O cep digitado não é válido.");
+                    }
+                    String json = response.body();
+                    CepEndereco cepEndereco = gson.fromJson(json, CepEndereco.class);
+                    meuEndereco = new Endereco(cepEndereco);
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+                    System.out.println("O endereço é: Rua " + meuEndereco.getLogradouro() + ", " + meuEndereco.getBairro() +
+                            " - " + meuEndereco.getCidade() + ", " + meuEndereco.getEstado());
 
-        String json = response.body();
+                    System.out.println("Deseja salvar o endereço?");
+                    System.out.println("1 - Salvar \n0 - Não");
+                    int opcao = ler.nextInt();
 
-
-        CepEndereco cepEndereco = gson.fromJson(json, CepEndereco.class);
-        meuEndereco = new Endereco(cepEndereco);
-
-        System.out.println("O endereço é: Rua " + meuEndereco.getLogradouro() + ", " + meuEndereco.getBairro() +
-                " - " + meuEndereco.getCidade() + ", " + meuEndereco.getEstado());
-
-        while (opcao != 0) {
-            System.out.println("Deseja salvar o endereço?");
-            System.out.println("1 - Salvar \n0 - Sair");
-            opcao = ler.nextInt();
-            break;
-        }
-
-
-        if(meuEndereco != null && opcao == 1) {
-            try (FileWriter writer = new FileWriter("endereco.json")) {
-                writer.write(gson.toJson(meuEndereco));
-                System.out.println("O endereço foi salvo no arquivo 'endereco.json");
-            } catch (IOException e) {
-                System.out.println("Erro ao salvar o endereço no arquivo: " + e.getMessage());
+                    if (meuEndereco != null && opcao == 1) {
+                        try (FileWriter writer = new FileWriter("endereco.json")) {
+                            writer.write(gson.toJson(meuEndereco));
+                            System.out.println("O endereço foi salvo no arquivo 'endereco.json");
+                        } catch (IOException e) {
+                            System.out.println("Erro ao salvar o endereço no arquivo: " + e.getMessage());
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
-
-
     }
 }
